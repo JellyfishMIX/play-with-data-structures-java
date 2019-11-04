84459c60c2f53d661a5a8fdbbac7824cf8c65dcc
JellyfishMIX <2273516339@qq.com>
2019-11-04 20:42:04 +0800

    :sparkles: Add BST algorithm(base node)

---
+++ src/com/algorithm/bst/Bst.java
@@ -0,0 +1,30 @@
+package com.algorithm.bst;
+
+public class Bst<E extends Comparable<E>>{
+    private class Node {
+        public E e;
+        public Node left, right;
+
+        public Node(E e) {
+            this.e = e;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    private Node root;  // 根节点
+    private int size;   // 二分搜索树节点步长
+
+    public Bst() {
+        this.root = null;
+        this.size = 0;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
