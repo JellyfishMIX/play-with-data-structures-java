6608b0e9203cd7f29931705a2f184de8c608da57
JellyfishMIX <2273516339@qq.com>
2020-06-12 22:17:54 +0800

    :construction: Updated BinarySearchTree with "add" method

--- src/com/algorithm/binarysearchtree/BinarySearchTree.java
+++ src/com/algorithm/binarysearchtree/BinarySearchTree.java
@@ -27,4 +27,33 @@ public class BinarySearchTree<E extends Comparable<E>>{
     public boolean isEmpty() {
         return size == 0;
     }
+
+    /**
+     * 向二分搜索树中添加新元素
+     * @param e element
+     */
+    public void add(E e) {
+        root = this.add(root, e);
+    }
+
+    /**
+     * 向以node为根的二分搜索树中插入元素E，递归
+     * @param node node
+     * @param e element
+     * @return 新的节点
+     */
+    private Node add(Node node, E e) {
+        if (node == null) {
+            size++;
+            return new Node(e);
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = this.add(node.left, e);
+        } else if (e.compareTo(node.e) > 0){
+            node.right = this.add(node.right, e);
+        }
+
+        return node;
+    }
 }
